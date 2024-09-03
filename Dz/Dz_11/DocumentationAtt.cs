967101acb9d8448002d597553e35985076d57552
user <user@WIN-MKQ26SV0D4C>
2024-09-03 20:11:19 +0300

    s

---
+++ Dz/Dz_11/DocumentationAtt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Dz_11;
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class DocumentationAttribute : Attribute
+{
+    public string Author { get; }
+    public int Version { get; }
+    public string Description { get; }
+    public DocumentationAttribute(string author, int version)
+    {  Author = author; Version = version; }
+    public DocumentationAttribute(string author, int version, string description) 
+    { Author = author; Version = version; Description = description; }
+}
---
+++ Dz/Dz_11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+namespace Dz_11;
+
+internal class Program
+{
+    [DocumentationAttribute("AuthorTest", 1)]
+    public void Method() { }
+    [DocumentationAttribute("AuthorTest2", 2, "TestDescription")]
+    public void Method2() { }
+    public static void Main(string[] args)
+    {
+        Type type = typeof(Program);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+        foreach (MethodInfo method in methods)
+        {
+            var attributes = method.GetCustomAttributes(typeof(DocumentationAttribute), false);
+            foreach (DocumentationAttribute attribute in attributes)
+            {
+                Console.WriteLine($"Method: {method.Name}");
+                Console.WriteLine($"Author: {attribute.Author}");
+                Console.WriteLine($"Version: {attribute.Version}");
+                Console.WriteLine($"Description: {attribute.Description}");
+            }
+        }
+        ExecuteByAuthor("AuthorTest");
+    }
+    public static void ExecuteByAuthor(string author)
+    {
+        Type type = typeof(Program);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+
+        foreach (MethodInfo method in methods)
+        {
+            var attributes = method.GetCustomAttributes(typeof(DocumentationAttribute), false);
+            foreach (DocumentationAttribute attribute in attributes)
+            {
+                if (attribute.Author == author)
+                {
+                    Console.WriteLine($"Executing Method: {method.Name}");
+                    method.Invoke(Activator.CreateInstance(type), null);
+                }
+            }
+        }
+    }
+}
