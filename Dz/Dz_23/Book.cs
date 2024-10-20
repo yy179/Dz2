437a6ae87fa9c553dc375277d6b82537a2015992
user <user@WIN-MKQ26SV0D4C>
2024-10-20 14:01:48 +0300

    s

---
+++ Dz/Dz_23/Book.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dz_23
+{
+    public class Book
+    {
+        private static Random random = new Random();
+        private static HashSet<int> existingId = new HashSet<int>();
+        private int id;
+        private string title;
+        private string description;
+        private int price;
+        public Book(string title, string description, int price)
+        {
+            Id = Generate();
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Cant be empty");
+                title = value;
+            }
+        }
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Cant be empty");
+                description = value;
+            }
+        }
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a positive number.")]
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+            }
+        }
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (!existingId.Add(value))
+                    throw new ArgumentException("Id must be unique, plese regenerate");
+                id = value;
+            }
+        }
+        public int Generate() 
+        {
+            int newId;
+            do
+            {
+                newId = random.Next(1,1000);
+            } while (!existingId.Add(newId));
+            return newId;
+        }
+    }
+}
---
+++ Dz/Dz_23/Controllers/BookController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dz_23.Controllers
+{
+    public class BookController : Controller
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book ("Astral", "Fantasy book", 8 ),
+            new Book ("Smith", "Romanic book", 10)
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(u => u.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> CreateBook([FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                book.Id = book.Generate();
+                books.Add(book);
+                return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingBook = books.FirstOrDefault(b => b.Id == id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                existingBook.Title = updatedBook.Title;
+                existingBook.Description = updatedBook.Description;
+                existingBook.Price = updatedBook.Price;
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                books.Remove(book);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
